e81a8ebebccac615d05529303985fe64ca727a98
Omhet <omhetehmo@yandex.ru>
2019-02-02 18:49:35 +0300

    Updates

---
+++ precache-manifest.93282fe8421da7daa81e7688fbf4a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50f04738be5d2c4447a4",
+    "url": "/static/js/main.50f04738.chunk.js"
+  },
+  {
+    "revision": "846dfff3a7cf9dde0406",
+    "url": "/static/js/1.846dfff3.chunk.js"
+  },
+  {
+    "revision": "50f04738be5d2c4447a4",
+    "url": "/static/css/main.b753edaf.chunk.css"
+  },
+  {
+    "revision": "846dfff3a7cf9dde0406",
+    "url": "/static/css/1.948aa3a1.chunk.css"
+  },
+  {
+    "revision": "afab174478b15c809607c173e7dc2d2a",
+    "url": "/index.html"
+  }
+];
